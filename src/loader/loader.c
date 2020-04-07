b71e845f536370cfc72e43bf02233b8ce951ce0d
RusuGabriel <rusugabriel13@gmail.com>
2020-04-07 22:41:08 +0300

    corrected a memory address typo

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -55,7 +55,7 @@ bool find(void *pageAddress, Loader *loader)
 	Page *cachedPage = loader->cachedPages;
 	while (cachedPage)
 	{
-		if ((pageAddress - cachedPage->pageAddress) <= loader->pageSize && (pageAddress - cachedPage->pageAddress) >= 0)
+		if (((char*)pageAddress - (char*)cachedPage->pageAddress) <= loader->pageSize && (pageAddress - cachedPage->pageAddress) >= 0)
 			return true;
 		cachedPage = cachedPage->nextPage;
 	}
